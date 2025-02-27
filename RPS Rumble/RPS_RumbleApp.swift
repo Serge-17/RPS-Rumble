e6a8d5253376a24af4d5a27e04d3c44e5e7216c7
Sergey Eliseev <sergei.eliseev17@icloud.com>
2025-02-27 12:45:04 +0300

    Initial Commit

---
+++ RPS Rumble/RPS_RumbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPS_RumbleApp.swift
+//  RPS Rumble
+//
+//  Created by Serge Eliseev on 27.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct RPS_RumbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
